62d8a8970bf31f6aa54f546f690940020cb1c57d
Henrique Melo <hlsmelosantander@gmail.com>
2023-07-10 11:42:05 -0300

    feature/cart - Fix sale flow

--- app/DB/Models/Sale.php
+++ app/DB/Models/Sale.php
@@ -2,6 +2,7 @@
 
 namespace Work\Soft_Expert\DB\Models;
 
+use Symfony\Component\VarDumper\VarDumper;
 use Work\Soft_Expert\DB\DB;
 
 class Sale {
@@ -21,16 +22,18 @@ class Sale {
         $sale_id = DB::execute($query);
         $sale_id = count($sale_id) > 0 ? $sale_id[0]['id'] : 0;
         $cart = json_decode($data['cart'], true);
-
+        
         foreach ($cart as $item) {
             $item['sale_id'] = $sale_id;
             unset($item['product']);
 
             
-            if ( Sale_Product::create($item) ) {
+            if ( !Sale_Product::create($item) ) {
                 return false;
             }
         }
+
+        return true;
     }
     
     static public function update(array $data) {
